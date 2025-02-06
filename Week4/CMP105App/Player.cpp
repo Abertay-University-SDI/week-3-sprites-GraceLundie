d67e7460b03ea2d75855e7968364d87d6116c916
2401945 <2401945@uad.ac.uk>
2025-02-06 11:54:02 +0000

    Lab 3
    Created Player class and header

---
+++ Week4/CMP105App/Player.cpp
@@ -0,0 +1,18 @@
+#include "Player.h"
+
+Player::Player() {
+	
+}
+
+Player::~Player() {
+
+}
+
+void Player::handleInput(float dt) {
+
+	if (input->isKeyDown(sf::Keyboard::Up))
+	{
+
+	}
+}
+
---
+++ Week4/CMP105App/Player.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Framework/BaseLevel.h"
+#include "Framework/Input.h"
+#include "Framework/GameObject.h"
+#include <string.h>
+#include <iostream>
+
+class Player : public GameObject {
+
+	public:
+		Player();
+		~Player();
+
+		void handleInput(float dt) override;
+};
